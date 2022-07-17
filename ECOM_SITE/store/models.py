3a5df16bf9698458b4ecb1c2060c5b8a4e3af372
Shipu <shipankar.shipu@gmail.com>
2022-07-17 19:06:44 +0600

    Adding Storefront from the resources

--- ECOM_SITE/store/models.py
+++ ECOM_SITE/store/models.py
@@ -35,7 +35,7 @@ class Customers(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=12)
+    phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE
